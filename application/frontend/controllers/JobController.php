b9aeefed025ea72586357a5eecd453ed727128bd
Phillip Shipley <phillip_shipley@sil.org>
2015-09-25 11:14:27 -0400

    added new basic User model and added auth rules to controllers

--- application/frontend/controllers/JobController.php
+++ application/frontend/controllers/JobController.php
@@ -2,6 +2,12 @@
 namespace frontend\controllers;
 
 use yii\rest\ActiveController;
+use yii\helpers\ArrayHelper;
+use yii\filters\auth\CompositeAuth;
+use yii\filters\auth\HttpBearerAuth;
+use yii\filters\AccessControl;
+use yii\web\ForbiddenHttpException;
+use yii\web\UnauthorizedHttpException;
 
 /**
  * Job controller
@@ -9,4 +15,32 @@ use yii\rest\ActiveController;
 class JobController extends ActiveController
 {    
     public $modelClass = 'common\models\Job';
+
+    public function behaviors()
+    {
+        return ArrayHelper::merge(parent::behaviors(), [
+            'authenticator' => [
+                'class' => CompositeAuth::className(),
+                'authMethods' => [
+                    HttpBearerAuth::className(), // Use header ... Authorization: Bearer abc123
+                ],
+            ],
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['@'], // Any logged in user
+                    ],
+                ],
+                'denyCallback' => function($rule, $action){
+                    if(\Yii::$app->user->isGuest){
+                        throw new UnauthorizedHttpException();
+                    } else {
+                        throw new ForbiddenHttpException();
+                    }
+                },
+            ]
+        ]);
+    }
 }
