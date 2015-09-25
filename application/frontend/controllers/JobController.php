b69c20b3fa08bc7cfdc54c0ed5abb94c56841c1f
Chris Hubbard <chris_hubbard@sil.org>
2015-09-25 10:38:36 -0400

    Add routes to get build information for job

--- application/frontend/controllers/JobController.php
+++ application/frontend/controllers/JobController.php
@@ -1,6 +1,9 @@
 <?php
 namespace frontend\controllers;
 
+use common\models\Job;
+use common\models\Build;
+
 use yii\rest\ActiveController;
 
 /**
@@ -9,4 +12,17 @@ use yii\rest\ActiveController;
 class JobController extends ActiveController
 {    
     public $modelClass = 'common\models\Job';
+    
+    public function actionLatestBuild($id) {
+        $job = Job::findById($id);
+        return $job->getLatestBuild();
+    }
+    
+    public function actionViewBuild($id, $build_id) {
+       $build = Build::findOne(['id' => $build_id, 'job_id' => $id]);
+       if (!$build){
+           throw new \yii\web\NotFoundHttpException();
+       }
+       return $build;
+    }
 }
