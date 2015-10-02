f795f723caee80deab1f18540290a0216ff6c447
Chris Hubbard <chris_hubbard@sil.org>
2015-10-02 14:33:58 -0400

    Update JobController
    * change status code and response for actionNewBuild
    * add some error checking

--- application/frontend/controllers/JobController.php
+++ application/frontend/controllers/JobController.php
@@ -10,7 +10,10 @@ use yii\filters\auth\CompositeAuth;
 use yii\filters\auth\HttpBearerAuth;
 use yii\filters\AccessControl;
 use yii\web\ForbiddenHttpException;
+use yii\web\NotFoundHttpException;
+use yii\web\ServerErrorHttpException;
 use yii\web\UnauthorizedHttpException;
+
 
 /**
  * Job controller
@@ -21,24 +24,38 @@ class JobController extends ActiveController
     
     public function actionLatestBuild($id) {
         $job = Job::findById($id);
-        return $job->getLatestBuild();
+        if (!$job){
+            throw new NotFoundHttpException("Job $id not found", 1443795485);
+        }
+
+        $build = $job->getLatestBuild();
+        if (!build){
+            throw new NotFoundHttpException("Lastest Build not found for Job $id", 1443797572);
+        }
+
+        return $build;
     }
     
     public function actionViewBuild($id, $build_id) {
        $build = Build::findOne(['id' => $build_id, 'job_id' => $id]);
        if (!$build){
-           throw new \yii\web\NotFoundHttpException();
+           throw new NotFoundHttpException();
        }
        return $build;
     }
     
     public function actionNewBuild($id) {
        $job = Job::findById($id);
+       if (!job){
+           throw new NotFoundHttpException("Job $id not found", 1443810472);
+       }
        $build = $job->createBuild();
        if (!$build){
-           throw new \yii\web\NotFoundHttpException();
+           throw new ServerErrorHttpException("Could not create Build for Job $id", 1443810508);
        }
-       return $build;
+
+       \Yii::$app->response->statusCode = 204;
+       return [];
     }
 
     public function behaviors()
