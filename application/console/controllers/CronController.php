2485b1df1a062b4c672652fe4f919abba6d553fa
Chris Hubbard <chris_hubbard@sil.org>
2015-10-22 15:50:27 -0400

    Update cron/get-builds-remaining to handle active without build_number

--- application/console/controllers/CronController.php
+++ application/console/controllers/CronController.php
@@ -270,13 +270,15 @@ class CronController extends Controller
         $complete = Build::STATUS_COMPLETED;
         foreach (Build::find()->where("status!='$complete'")->each(50) as $build){
             $jobName = $build->job->name();
-            $jenkinsBuild = $jenkins->getBuild($jobName, $build->build_number);
-            $buildResult = $jenkinsBuild->getResult();
-            $buildArtifact = $this->getArtifactUrl($jenkinsBuild);
-            $s3Url = $this->getS3Url($build, $jenkinsBuild);
-            echo "Job=$jobName, Number=$build->build_number, Status=$build->status\n"
-                . "  Build: Result=$buildResult, Artifact=$buildArtifact\n"
-                . "  S3: Url=$s3Url\n";
+            if ($build->build_number > 0) {
+                $jenkinsBuild = $jenkins->getBuild($jobName, $build->build_number);
+                $buildResult = $jenkinsBuild->getResult();
+                $buildArtifact = $this->getArtifactUrl($jenkinsBuild);
+                $s3Url = $this->getS3Url($build, $jenkinsBuild);
+                echo "Job=$jobName, Number=$build->build_number, Status=$build->status\n"
+                    . "  Build: Result=$buildResult, Artifact=$buildArtifact\n"
+                    . "  S3: Url=$s3Url\n";
+            }
         }
     }
     /**
