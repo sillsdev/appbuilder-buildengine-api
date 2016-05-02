0d018af9f14591f251ea65d52ecb936b81f86bae
Chris Hubbard <chris_hubbard@sil.org>
2016-05-02 13:27:19 -0400

    Add client table

--- application/rebuildbasemodels.sh
+++ application/rebuildbasemodels.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-TABLES=(job build release email_queue operation_queue )
+TABLES=(job build release email_queue operation_queue client)
 SUFFIX="Base"
 
 declare -A models
@@ -9,6 +9,7 @@ models["build"]="BuildBase"
 models["release"]="ReleaseBase"
 models["email_queue"]="EmailQueueBase"
 models["operation_queue"]="OperationQueueBase"
+models["client"]="ClientBase"
 
 for i in "${!models[@]}"; do
     CMD="./yii gii/model --tableName=$i --modelClass=${models[$i]} --generateRelations=1 --enableI18N=1 --overwrite=1 --interactive=0 --ns=\common\models"
