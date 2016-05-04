8e28b4f831e3b3b3fd5ba25f23f06e40ce254ada
Chris Hubbard <chris_hubbard@sil.org>
2016-05-04 15:13:46 -0400

    Save Error Logs for Aborted Builds too

--- application/console/components/ManageBuildsAction.php
+++ application/console/components/ManageBuildsAction.php
@@ -84,6 +84,7 @@ class ManageBuildsAction extends ActionCommon
                         $build->status = Build::STATUS_COMPLETED;
                         switch($build->result){
                             case JenkinsBuild::FAILURE:
+                            case JenkinsBuild::ABORTED:
                                 $task = OperationQueue::SAVEERRORTOS3;
                                 $build_id = $build->id;
                                 OperationQueue::findOrCreate($task, $build_id, "build");
