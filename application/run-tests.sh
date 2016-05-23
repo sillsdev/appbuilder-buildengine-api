51d26a27576da0823b12a788c558f6166776252b
David Moore <david_moore1@sil.org>
2016-05-23 14:20:23 -0400

    Adjust run-test.sh scripts for codeship

--- application/run-tests.sh
+++ application/run-tests.sh
@@ -1,22 +1,10 @@
 #!/usr/bin/env bash
 
 # Run database migrations
-#/data/yii migrate --interactive=0
-#/data/yii migrate --interactive=0 --migrationPath=console/migrations-test
-
-# Run codeception tests
-#cd /data
-#echo "Installing dev dependencies..."
-#composer install --prefer-dist --no-interaction
-#./vendor/bin/codecept run unit
-
-#!/usr/bin/env bash
-
-# Run database migrations
 whenavail db 3306 100 /data/yii migrate --interactive=0
 whenavail db 3306 100 /data/yii migrate --interactive=0 --migrationPath=console/migrations-test
 
 # Run codeception tests
 cd /data
 composer install --prefer-dist --no-interaction
-./vendor/bin/codecept.phar run unit
+./vendor/bin/codecept run unit
