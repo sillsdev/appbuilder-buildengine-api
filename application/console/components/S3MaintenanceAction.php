338a9701c25fbb63923a6d4dca8755da3a5d42cf
David Moore <david_moore1@sil.org>
2016-07-21 14:26:56 -0400

    Fix s3 maintenance action call
    Cron task was not working because of a $ instead of an &

--- application/console/components/S3MaintenanceAction.php
+++ application/console/components/S3MaintenanceAction.php
@@ -6,12 +6,16 @@ use common\models\Job;
 use common\components\S3;
 use common\components\Appbuilder_logger;
 
+use common\helpers\Utils;
+
 class S3MaintenanceAction
 {
     private $s3;
     public function performAction()
     {
         $logger = new Appbuilder_logger("S3MaintenanceAction");
+        $prefix = Utils::getPrefix();
+        echo "[$prefix] S3MaintenanceAction: Started". PHP_EOL;
         // Clean up S3 files
         $jobNames = Job::getJobNames();
         $this->s3 = new S3();
