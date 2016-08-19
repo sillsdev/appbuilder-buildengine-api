1cf6633637b6068128436a76791f96f5fff54009
Chris Hubbard <chris@thehubbards.org>
2016-08-19 10:59:25 -0400

    Change format of fonts.xml

--- application/console/views/cron/scripts/bloomappmaker/build/appbuilder-fonts.rb
+++ application/console/views/cron/scripts/bloomappmaker/build/appbuilder-fonts.rb
@@ -11,44 +11,53 @@ class AppBuilderFont
 end
 
 class AppBuilderFonts
-  attr_accessor :fonts
+  attr_accessor :fontmap
 
   def initialize
-    self.fonts = []
+    self.fontmap = Hash.new
   end
 
   def add_font(family_name, file_name, bold = false, italic = false)
     weight = bold ? 'bold' : 'normal'
     style = italic ? 'italic' : 'normal'
     font = AppBuilderFont.new(family_name, file_name, weight, style)
-    self.fonts << font
+    unless self.fontmap.has_key?(family_name)
+      self.fontmap[family_name] = []
+    end
+    self.fontmap[family_name] << font
   end
 
   def to_xml
-    builder = Nokogiri::XML::Builder.new { |xml|
+    builder = Nokogiri::XML::Builder.new do |xml|
       xml.fonts {
-        @fonts.each do |font|
-          xml.send('font', :family => font.familyName) {
-            xml.filename font.fileName
-            xml.send('style-spec', 'property' => 'font-weight', 'value' => font.weight)
-            xml.send('style-spec', 'property' => 'font-style', 'value' => font.style)
+        @fontmap.each do |family_name, fonts|
+          xml.send('font-family', :name => family_name) {
+            fonts.each do |font|
+              xml.font {
+                xml.filename font.fileName
+                xml.send('style-spec', 'property' => 'font-weight', 'value' => font.weight)
+                xml.send('style-spec', 'property' => 'font-style', 'value' => font.style)
+              }
+            end
           }
         end
       }
-    }
+    end
     builder.to_xml
   end
 
   def count
-    return self.fonts.count
+    return self.fontmap.reduce(0) {|sum, (name, fonts)| sum += fonts.count}
   end
 end
 
+# fontdir = '/usr/share/fonts/truetype/msttcorefonts'
 # fs = AppBuilderFonts.new
-# fs.add_font('Comic Sans', 'comic.ttf')
-# fs.add_font('Comic Sans', 'comicbd.ttf', true)
-# fs.add_font('Times New Roman', 'times.ttf')
-# fs.add_font('Times New Roman', 'timesbd.ttf', true)
-# fs.add_font('Times New Roman', 'timesi.ttf', false, true)
-# fs.add_font('Times New Roman', 'timesbi.ttf', true, true)
+# fs.add_font('Comic Sans', "#{fontdir}/comic.ttf")
+# fs.add_font('Comic Sans', "#{fontdir}/comicbd.ttf", true)
+# fs.add_font('Times New Roman', "#{fontdir}/times.ttf")
+# fs.add_font('Times New Roman', "#{fontdir}/timesbd.ttf", true)
+# fs.add_font('Times New Roman', "#{fontdir}/timesi.ttf", false, true)
+# fs.add_font('Times New Roman', "#{fontdir}/timesbi.ttf", true, true)
+# puts "Count: #{fs.count}"
 # File.write('fonts.xml', fs.to_xml)
