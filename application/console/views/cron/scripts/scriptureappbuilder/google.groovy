efe79f1911bf2ae1a0d5fa0dcca4276dd9f35401
David Moore <david_moore1@sil.org>
2016-10-21 08:36:27 -0400

    Remove obsolete code
    Remove old wycliffe only methods from groovy scripts

--- application/console/views/cron/scripts/scriptureappbuilder/google.groovy
+++ application/console/views/cron/scripts/scriptureappbuilder/google.groovy
@@ -2,18 +2,6 @@
 package scriptureappbuilder;
 
 class google {
-    static Closure wycliffeusa_credentialsBindingWrapper() {
-        return { project ->
-            project / 'buildWrappers' << 'org.jenkinsci.plugins.credentialsbinding.impl.SecretBuildWrapper' {
-                bindings {
-                    'org.jenkinsci.plugins.credentialsbinding.impl.FileBinding' {
-                        variable 'PAJ'
-                        credentialsId 'wycliffeusa-playstore-api-json'
-                    }
-                }
-            }
-        }
-    }
     static Closure credentialsBindingWrapper(publisherName) { 
          return { project -> 
             project / 'buildWrappers' << 'org.jenkinsci.plugins.credentialsbinding.impl.SecretBuildWrapper' { 
--- application/console/views/cron/scripts/scriptureappbuilder/keystore.groovy
+++ application/console/views/cron/scripts/scriptureappbuilder/keystore.groovy
@@ -1,31 +1,6 @@
 package scriptureappbuilder;
 
 class keystore {
-    static Closure wycliffeusa_credentialsBindingWrapper() {
-        return { project ->
-            project / 'buildWrappers' << 'org.jenkinsci.plugins.credentialsbinding.impl.SecretBuildWrapper' {
-                bindings {
-                    'org.jenkinsci.plugins.credentialsbinding.impl.FileBinding' {
-                        variable 'KS'
-                        credentialsId 'wycliffeusa-ks'
-                    }
-                    'org.jenkinsci.plugins.credentialsbinding.impl.StringBinding' {
-                        variable 'KSP'
-                        credentialsId 'wycliffeusa-ksp'
-                    }
-                    'org.jenkinsci.plugins.credentialsbinding.impl.StringBinding' {
-
-                        variable 'KA'
-                        credentialsId  'wycliffeusa-ka'
-                    }
-                    'org.jenkinsci.plugins.credentialsbinding.impl.StringBinding' {
-                        variable 'KAP'
-                        credentialsId 'wycliffeusa-kap'
-                    }
-                }
-            }
-        }
-    }
 
     static Closure credentialsBindingWrapper(publisherName) { 
         return { project ->
