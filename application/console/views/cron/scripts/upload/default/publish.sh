1311cb94a94ca6c796de4317cfbb024af750c25b
Chris Hubbard <chris@thehubbards.org>
2019-06-21 15:33:15 -0400

    Update format of s3_bucket/PUBLISHER/bucket
    * Format: bucket or bucket/to/path

--- application/console/views/cron/scripts/upload/default/publish.sh
+++ application/console/views/cron/scripts/upload/default/publish.sh
@@ -34,36 +34,26 @@ publish_s3_bucket() {
   CREDENTIALS="${SECRETS_DIR}/s3_bucket/${PUBLISHER}/credentials"
   CONFIG="${SECRETS_DIR}/s3_bucket/${PUBLISHER}/config"
   SRC_FILE="${APK_FILES[0]}"
-  DEST_BUCKET=$(cat "${SECRETS_DIR}/s3_bucket/${PUBLISHER}/bucket")
+  DEST_BUCKET_PATH=$(cat "${SECRETS_DIR}/s3_bucket/${PUBLISHER}/bucket")
   DEST_FILE="$(basename "${SRC_FILE}")"
 
-  scheme="$(parse_scheme "${DEST_BUCKET}")"
-  if [ "$scheme" != "s3" ]; then
-    echo "Invalid Bucket Path: ${DEST_BUCKET}" > "${OUTPUT_DIR}"/console.log
-    exit 1
-  fi
-
-  url_host="$(parse_host "${DEST_BUCKET}")"
-  url_path="$(parse_path "${DEST_BUCKET}")"
-
-  # if DEST_BUCKET=s3://foo : host= , path=//foo
-  # if DEST_BUCKET=s3://foo/path : host=foo, path=/path/to
-  if [[ "$url_host" == "" ]]; then
-    url="https:${url_path}.s3.amazonaws.com/${DEST_FILE}"
+  IFS=/ read -r DEST_BUCKET DEST_PATH <<< "${DEST_BUCKET_PATH}"
+  if [[ "${DEST_PATH}" == "" ]]; then
+    PUBLISH_URL="https:${DEST_BUCKET}.s3.amazonaws.com/${DEST_FILE}"
   else
-    url="https://${url_host}.s3.amazonaws.com${url_path}/${DEST_FILE}"
+    PUBLISH_URL="https://${DEST_BUCKET}.s3.amazonaws.com/${DEST_PATH}/${DEST_FILE}"
   fi
 
   echo "CREDENTIALS=${CREDENTIALS}"
   echo "CONFIG=${CONFIG}"
   echo "SRC_FILE=${SRC_FILE}"
-  echo "DEST_BUCKET=${DEST_BUCKET}"
+  echo "DEST_BUCKET=${DEST_BUCKET_PATH}"
   echo "DEST_FILE=${DEST_FILE}"
-  echo "URL=${url}"
+  echo "URL=${PUBLISH_URL}"
 
   AWS_SHARED_CREDENTIALS_FILE="${CREDENTIALS}" AWS_CONFIG_FILE="${CONFIG}" aws s3 cp "${SRC_FILE}" "${DEST_BUCKET}/${DEST_FILE}" |& tee -a "${OUTPUT_DIR}"/console.log
-  echo "${url}" > "${OUTPUT_DIR}"/publish_url.txt
 
+  echo "${PUBLISH_URL}" > "${OUTPUT_DIR}"/publish_url.txt
 }
 
 publish_gradle() {
@@ -78,64 +68,6 @@ publish_gradle() {
     popd || exit 1
   fi
 }
-
-# https://stackoverflow.com/a/45977232/35577
-#
-# Following regex is based on https://tools.ietf.org/html/rfc3986#appendix-B with
-# additional sub-expressions to split authority into userinfo, host and port
-#
-readonly URI_REGEX='^(([^:/?#]+):)?(//((([^:/?#]+)@)?([^:/?#]+)(:([0-9]+))?))?(/([^?#]*))(\?([^#]*))?(#(.*))?'
-#                    ↑↑            ↑  ↑↑↑            ↑         ↑ ↑            ↑ ↑        ↑  ↑        ↑ ↑
-#                    |2 scheme     |  ||6 userinfo   7 host    | 9 port       | 11 rpath |  13 query | 15 fragment
-#                    1 scheme:     |  |5 userinfo@             8 :…           10 path    12 ?…       14 #…
-#                                  |  4 authority
-#                                  3 //…
-
-parse_scheme () {
-    # shellcheck disable=SC2199
-    [[ "$@" =~ $URI_REGEX ]] && echo "${BASH_REMATCH[2]}"
-}
-
-parse_authority () {
-    # shellcheck disable=SC2199
-    [[ "$@" =~ $URI_REGEX ]] && echo "${BASH_REMATCH[4]}"
-}
-
-parse_user () {
-    # shellcheck disable=SC2199
-    [[ "$@" =~ $URI_REGEX ]] && echo "${BASH_REMATCH[6]}"
-}
-
-parse_host () {
-    # shellcheck disable=SC2199
-    [[ "$@" =~ $URI_REGEX ]] && echo "${BASH_REMATCH[7]}"
-}
-
-parse_port () {
-    # shellcheck disable=SC2199
-    [[ "$@" =~ $URI_REGEX ]] && echo "${BASH_REMATCH[9]}"
-}
-
-parse_path () {
-    # shellcheck disable=SC2199
-    [[ "$@" =~ $URI_REGEX ]] && echo "${BASH_REMATCH[10]}"
-}
-
-parse_rpath () {
-    # shellcheck disable=SC2199
-    [[ "$@" =~ $URI_REGEX ]] && echo "${BASH_REMATCH[11]}"
-}
-
-parse_query () {
-    # shellcheck disable=SC2199
-    [[ "$@" =~ $URI_REGEX ]] && echo "${BASH_REMATCH[13]}"
-}
-
-parse_fragment () {
-    # shellcheck disable=SC2199
-    [[ "$@" =~ $URI_REGEX ]] && echo "${BASH_REMATCH[15]}"
-}
-
 
 APK_FILES=( "${ARTIFACTS_DIR}"/*.apk )
 
