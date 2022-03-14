8ae120a00d2ea35154037c428802161b4000b12b
Chris Hubbard <chris@thehubbards.org>
2022-03-14 14:59:03 -0400

    Change how shellcheck is installed
    * The version of shellcheck installed with the OS is WAAAY out
      of date.
    * Shellcheck only needs to be installed when running tests. So
      Remove from Dockerfile and install latest version when running
      tests.

--- application/run-tests.sh
+++ application/run-tests.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 # Run shell tests
+scversion="stable" # or "v0.4.7", or "latest"
+curl -s -L --output - "https://github.com/koalaman/shellcheck/releases/download/${scversion?}/shellcheck-${scversion?}.linux.x86_64.tar.xz" | tar -xJv
+cp "shellcheck-${scversion}/shellcheck" /usr/bin/
+shellcheck --version
 shellcheck /data/console/views/cron/scripts/upload/default/*.sh || exit 1
 
 # Run database migrations
