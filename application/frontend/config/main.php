e52792e28cd7d4d552b0d8ac0f94451b9e40cb60
Chris Hubbard <chris@thehubbards.org>
2022-03-17 15:32:25 -0400

    Replace composer-asset-plugin with asset-packagist
    * Switch to Composer 2

--- application/frontend/config/main.php
+++ application/frontend/config/main.php
@@ -11,6 +11,10 @@ return [
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'controllerNamespace' => 'frontend\controllers',
+    'aliases' => [
+        "@bower" => "@vendor/bower-asset",
+        "@npm" => "@vendor/npm-asset"
+    ],
     'components' => [
         'user' => [
             'identityClass' => 'common\models\User',
