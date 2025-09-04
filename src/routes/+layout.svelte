e095c3fe3a432c40e8dbc2e435e67a3208d3e5fc
Aidan Jones <aejones4gm@gmail.com>
2025-09-04 16:09:57 -0500

    npx sv create

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import '../app.css';
+	import favicon from '$lib/assets/favicon.ico';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<title>SIL AppBuilder Administration</title>
+</svelte:head>
+
+{@render children?.()}
