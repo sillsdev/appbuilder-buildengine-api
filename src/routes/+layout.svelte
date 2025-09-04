90c6b71f3e392b308e7bf237d8f233a71e09be6f
Aidan Jones <aejones4gm@gmail.com>
2025-09-04 17:31:04 -0500

    Basic structure
    Home and About pages
    
    Stubs for everything else

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.ico';
+	import { page } from '$app/state';
 
 	let { children } = $props();
+
+	function isUrlActive(route: string) {
+		return page.url.pathname === route;
+	}
 </script>
 
 <svelte:head>
@@ -10,4 +15,33 @@
 	<title>SIL AppBuilder Administration</title>
 </svelte:head>
 
-{@render children?.()}
+<header class="bg-primary text-primary-content">
+	<nav class="navbar">
+		<div class="flex-1">
+			<a class="btn text-xl btn-ghost" href="/">SIL International</a>
+		</div>
+		<div class="flex-none">
+			<ul class="menu menu-horizontal px-1">
+				<li><a href="/" class:bg-secondary={isUrlActive("/")}>Home</a></li>
+				<li><a href="/about" class:bg-secondary={isUrlActive("/about")}>About</a></li>
+				<li><a href="/contact" class:bg-secondary={isUrlActive("/contact")}>Contact</a></li>
+			</ul>
+		</div>
+	</nav>
+</header>
+<main class="mx-auto w-3xl max-w-full bg-base-100 px-10 text-base-content md:w-5xl grow">
+	{@render children?.()}
+</main>
+<footer class="flex flex-col sm:flex-row bg-base-200 text-base-content w-full sm:place-items-center">
+	<div class="grow">© SIL International {new Date().getFullYear()}</div>
+	<div>
+		Powered by&nbsp;<a href="https://kit.svelte.dev" rel="external" class="link">SvelteKit</a>
+	</div>
+</footer>
+
+<style>
+	footer {
+		border-top: 1px solid #ddd;
+		padding: 20px;
+	}
+</style>
---
+++ src/routes/release-admin/+page.svelte
@@ -0,0 +1,2 @@
+<!-- TODO -->
+Under construction
