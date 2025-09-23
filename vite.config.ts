3909345f674aaf04c41b5b5849915d18ea3f970f
Aidan Jones <aidan_jones@sil.org>
2025-09-23 11:50:32 -0500

    Host dev on 8443

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	server: {
+		port: 8443
+	},
+	preview: {
+		port: 8443
+	},
 	plugins: [tailwindcss(), sveltekit()]
 });
