22eb41eafdc51af99f90445388ac6b5a64f277e1
Aidan Jones <aidan_jones@sil.org>
2025-09-29 16:42:29 -0500

    Add ErrorResponse utility function

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,26 +1,15 @@
 import { type Handle } from '@sveltejs/kit';
 import { prisma } from '$lib/server/prisma';
+import { ErrorResponse } from '$lib/utils';
 
 export const handle: Handle = async ({ event, resolve }) => {
   if (event.route.id?.split('/')[1] === '(api)') {
     if (event.request.headers.get('Content-Type') !== 'application/json') {
-      return new Response(
-        JSON.stringify({
-          code: 400,
-          error: 'Missing Header Content-Type: application/json'
-        }),
-        { status: 400 }
-      );
+      return ErrorResponse(400, 'Missing Header Content-Type: application/json');
     }
     const access_token = event.request.headers.get('Authorization')?.replace('Bearer ', '');
-    if (!(access_token && (await prisma.client.findFirst({ where: { access_token } })))) {
-      return new Response(
-        JSON.stringify({
-          code: 401,
-          error: 'Missing Header Authorization: Bearer <token>'
-        }),
-        { status: 401 }
-      );
+    if (!access_token) {
+      return ErrorResponse(401, 'Missing Header Authorization: Bearer <token>');
     }
   }
   return resolve(event);
