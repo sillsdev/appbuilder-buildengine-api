16dfe8476193b798a4c75500cf65051bbf711b6d
Aidan Jones <aidan_jones@sil.org>
2025-10-14 16:47:07 -0500

    CodeCommit wrapper

---
+++ src/lib/server/aws/codecommit.ts
@@ -0,0 +1,81 @@
+import {
+  CodeCommitClient,
+  GetBranchCommand,
+  GetRepositoryCommand
+} from '@aws-sdk/client-codecommit';
+import { AWSCommon } from './common';
+
+export class CodeCommit extends AWSCommon {
+  public codeCommitClient;
+
+  public constructor() {
+    super();
+    this.codeCommitClient = CodeCommit.getCodeCommitClient();
+  }
+  /**
+   * Configure and get the CodeCommit Client
+   * @return \Aws\CodeBuild\CodeCommitClient
+   */
+  public static getCodeCommitClient() {
+    return new CodeCommitClient({
+      region: AWSCommon.getArtifactsBucketRegion()
+    });
+  }
+  /**
+   * Returns http url of code commit archive derived from git url needed for CodeBuild
+   *
+   * @param string git_url
+   * @return string http codecommit url
+   */
+  public async getSourceURL(git_url: string) {
+    console.log(`[${new Date().toISOString()}] getSourceURL URL: ${git_url}`);
+    const repo = git_url.substring(git_url.indexOf('/') + 1);
+    const repoInfo = await this.codeCommitClient.send(
+      new GetRepositoryCommand({
+        repositoryName: repo
+      })
+    );
+    const cloneUrl = repoInfo.repositoryMetadata?.cloneUrlHttp;
+    console.log(`cloneUrl: ${cloneUrl}`);
+    return cloneUrl;
+  }
+  /**
+   * Return ssh url of code commit archive derived from git url needed for CodeBuild
+   *
+   * @param string git_url
+   * @return string http codecommit url
+   */
+  public async getSourceSshURL(git_url: string) {
+    console.log(`[${new Date().toISOString()}] getSourceURL URL: ${git_url}`);
+    const repo = git_url.substring(git_url.indexOf('/') + 1);
+    const repoInfo = await this.codeCommitClient.send(
+      new GetRepositoryCommand({
+        repositoryName: repo
+      })
+    );
+    const cloneUrl = repoInfo.repositoryMetadata?.cloneUrlSsh;
+    console.log(`cloneUrl: ${cloneUrl}`);
+    return cloneUrl;
+  }
+
+  /**
+   *  Returns commit id of the specified branch for the specified repo
+   *
+   * @param string git_url
+   * @param string branch
+   * @return string commit id
+   */
+  public async getCommitId(git_url: string, branch: string) {
+    console.log(`[${new Date().toISOString()}] getCommitId URL: ${git_url} Branch: ${branch}`);
+    const repo = git_url.substring(git_url.indexOf('/') + 1);
+    const result = await this.codeCommitClient.send(
+      new GetBranchCommand({
+        branchName: branch,
+        repositoryName: repo
+      })
+    );
+    const commitId = result.branch?.commitId;
+    console.log(`commitId: ${commitId}`);
+    return commitId;
+  }
+}
