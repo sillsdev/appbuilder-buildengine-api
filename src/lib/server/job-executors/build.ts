5d4eda41386c1a908674c63881544334e5f46ea1
Aidan Jones <aidan_jones@sil.org>
2025-10-23 15:25:51 -0500

    Build task

--- src/lib/server/job-executors/build.ts
+++ src/lib/server/job-executors/build.ts
@@ -1,10 +1,144 @@
+import type { Prisma } from '@prisma/client';
 import type { Job } from 'bullmq';
+import { readFile } from 'node:fs/promises';
+import { Build } from '../../models/build';
+import { CodeBuild } from '../aws/codebuild';
+import { CodeCommit } from '../aws/codecommit';
 import type { BullMQ } from '../bullmq';
+import { prisma } from '../prisma';
 
 export async function product(job: Job<BullMQ.Build.Product>): Promise<unknown> {
-  return;
+  try {
+    const build = await prisma.build.findUniqueOrThrow({
+      where: {
+        id: job.data.buildId
+      },
+      include: {
+        job: true
+      }
+    });
+    job.updateProgress(10);
+
+    // Don't start job if a job for this build is currently running
+    const builds = await prisma.build.count({
+      where: {
+        job_id: build.job_id,
+        status: { in: [Build.Status.Active, Build.Status.PostProcessing] }
+      }
+    });
+    if (builds > 0) {
+      job.log('Existing active builds found. Build cancelled');
+      // TODO retry after???
+      return { existing: builds };
+    }
+    const gitUrl = build.job.git_url;
+    // Check to see if codebuild project
+    const codeCommitProject = gitUrl.startsWith('ssh://');
+    if (codeCommitProject) {
+      job.log('Starting build with CodeCommit');
+      // Left this block intact to make it easier to remove when codecommit is not supported
+      const codecommit = new CodeCommit();
+      const branch = 'master';
+      const repoUrl = await codecommit.getSourceURL(gitUrl);
+      if (!repoUrl) throw new Error('No repoUrl found!');
+      const commitId = await codecommit.getCommitId(gitUrl, branch);
+      if (!commitId) throw new Error('No commitId found!');
+      job.updateProgress(25);
+
+      const script = (await readFile('scripts/appbuilders_build.yml')).toString();
+      job.updateProgress(50);
+      // Start the build
+      const codeBuild = new CodeBuild();
+      const versionCode = (await getVersionCode(build.job)) + 1;
+      const lastBuildGuid = await codeBuild.startBuild(
+        repoUrl,
+        commitId,
+        build,
+        script,
+        versionCode,
+        codeCommitProject
+      );
+      job.updateProgress(75);
+      if (lastBuildGuid) {
+        await prisma.build.update({
+          where: { id: build.id },
+          data: {
+            build_guid: lastBuildGuid,
+            codebuild_url: CodeBuild.getCodeBuildUrl('build_app', lastBuildGuid),
+            console_text_url: CodeBuild.getConsoleTextUrl('build_app', lastBuildGuid),
+            status: Build.Status.Active
+          }
+        });
+      }
+      job.updateProgress(100);
+      return {
+        repoUrl,
+        commitId,
+        versionCode,
+        lastBuildGuid
+      };
+    } else {
+      job.log('Starting build with CodeBuild');
+      const script = (await readFile('scripts/appbuilders_s3_build.yml')).toString();
+      job.updateProgress(50);
+      // Start the build
+      const codeBuild = new CodeBuild();
+      const commitId = ''; // TODO: Remove when git is removed
+      const versionCode = await getVersionCode(build.job); // Is there a reason this is not incremented here??
+      const lastBuildGuid = await codeBuild.startBuild(
+        gitUrl,
+        commitId,
+        build,
+        script,
+        versionCode,
+        codeCommitProject
+      );
+      job.updateProgress(75);
+      if (lastBuildGuid) {
+        await prisma.build.update({
+          where: { id: build.id },
+          data: {
+            build_guid: lastBuildGuid,
+            codebuild_url: CodeBuild.getCodeBuildUrl('build_app', lastBuildGuid),
+            console_text_url: CodeBuild.getConsoleTextUrl('build_app', lastBuildGuid),
+            status: Build.Status.Active
+          }
+        });
+      }
+      job.updateProgress(100);
+      return {
+        versionCode,
+        lastBuildGuid
+      };
+    }
+  } catch (e) {
+    job.log(`${e}`);
+    await prisma.build.update({
+      where: { id: job.data.buildId },
+      data: {
+        result: Build.Result.Failure,
+        status: Build.Status.Completed
+      }
+    });
+  }
 }
 
 export async function postProcess(job: Job<BullMQ.Build.PostProcess>): Promise<unknown> {
   return;
+}
+
+async function getVersionCode(
+  job: Prisma.jobGetPayload<{ select: { id: true; existing_version_code: true } }>
+) {
+  const build = await prisma.build.aggregate({
+    where: {
+      job_id: job.id,
+      status: Build.Status.Completed,
+      result: Build.Result.Success
+    },
+    _max: {
+      version_code: true
+    }
+  });
+  return build._max.version_code ?? job.existing_version_code ?? 0;
 }
--- src/lib/server/job-executors/polling.ts
+++ src/lib/server/job-executors/polling.ts
@@ -1,10 +1,93 @@
+import type { Prisma } from '@prisma/client';
 import type { Job } from 'bullmq';
-import type { BullMQ } from '../bullmq';
+import { Build } from '../../models/build';
+import { CodeBuild } from '../aws/codebuild';
+import { BullMQ, getQueues } from '../bullmq';
+import { prisma } from '../prisma';
 
 export async function build(job: Job<BullMQ.Polling.Build>): Promise<unknown> {
-  return;
+  try {
+    const build = await prisma.build.findUnique({
+      where: { id: job.data.buildId },
+      include: { job: true }
+    });
+
+    if (build?.job) {
+      const codeBuild = new CodeBuild();
+      const buildStatus = await codeBuild.getBuildStatus(
+        build.build_guid!,
+        CodeBuild.getCodeBuildProjectName('build_app')
+      );
+      const phase = buildStatus?.currentPhase;
+      let status = buildStatus?.buildStatus;
+      job.log(` phase: ${phase} status: ${status}`);
+      if (codeBuild.isBuildComplete(buildStatus)) {
+        job.log(' Build Complete');
+      } else {
+        job.log(' Build Incomplete');
+      }
+
+      job.updateProgress(50);
+
+      if (codeBuild.isBuildComplete(buildStatus)) {
+        build.status = Build.Status.PostProcessing;
+        status = codeBuild.getStatus(buildStatus);
+        switch (status) {
+          case CodeBuild.Status.Failed:
+          case CodeBuild.Status.Fault:
+          case CodeBuild.Status.TimedOut:
+            build.result = Build.Result.Failure;
+            await handleFailure(build);
+            break;
+          case CodeBuild.Status.Stopped:
+            build.result = Build.Result.Aborted;
+            await handleFailure(build);
+            break;
+          case CodeBuild.Status.Succeeded:
+            await getQueues().S3.add(`Save Build ${job.data.buildId} to S3`, {
+              type: BullMQ.JobType.S3_CopyArtifacts,
+              scope: 'build',
+              id: job.data.buildId
+            });
+            break;
+        }
+      }
+      await prisma.build.update({
+        where: { id: build.id },
+        data: { ...build, job: undefined }
+      });
+      job.updateProgress(100);
+      return {
+        status: build.status,
+        guid: build.build_guid,
+        result: build.result,
+        url_base: build.artifact_url_base,
+        files: build.artifact_files
+      };
+    }
+  } catch (e) {
+    job.log(`${e}`);
+    await prisma.build.update({
+      where: { id: job.data.buildId },
+      data: {
+        result: Build.Result.Failure,
+        status: Build.Status.Completed
+      }
+    });
+  }
 }
 
 export async function publish(job: Job<BullMQ.Polling.Publish>): Promise<unknown> {
   return;
+}
+
+async function handleFailure(
+  build: Prisma.buildGetPayload<{ select: { error: true; id: true; console_text_url: true } }>
+) {
+  build.error = build.console_text_url;
+  await getQueues().S3.add(`Save Errors for Build ${build.id} to S3`, {
+    type: BullMQ.JobType.S3_CopyError,
+    scope: 'build',
+    id: build.id
+  });
 }
