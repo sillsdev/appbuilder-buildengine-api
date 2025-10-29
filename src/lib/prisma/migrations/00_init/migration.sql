fe76a439d085cad6f75b61edf8c78889ff0b168b
Aidan Jones <aidan_jones@sil.org>
2025-10-29 10:48:29 -0500

    Use PostgreSQL instead of MariaDB
    IMPORTANT!: Data will need to be manually migrated from MariaDB to PostgreSQL. I did it using CSV export/import (I did have to manually reset the id sequences).

--- src/lib/prisma/migrations/00_init/migration.sql
+++ src/lib/prisma/migrations/00_init/migration.sql
@@ -1,156 +1,123 @@
 -- CreateTable
-CREATE TABLE `build` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `job_id` INTEGER NOT NULL,
-    `status` VARCHAR(255) NULL,
-    `result` VARCHAR(255) NULL,
-    `error` VARCHAR(2083) NULL,
-    `created` DATETIME(0) NULL,
-    `updated` DATETIME(0) NULL,
-    `channel` VARCHAR(255) NULL,
-    `version_code` INTEGER NULL,
-    `artifact_url_base` VARCHAR(2083) NULL,
-    `artifact_files` VARCHAR(4096) NULL,
-    `build_guid` VARCHAR(255) NULL,
-    `console_text_url` VARCHAR(255) NULL,
-    `codebuild_url` VARCHAR(255) NULL,
-    `targets` VARCHAR(255) NULL,
-    `environment` TEXT NULL,
+CREATE TABLE "public"."build" (
+    "id" SERIAL NOT NULL,
+    "job_id" INTEGER NOT NULL,
+    "status" VARCHAR(255),
+    "result" VARCHAR(255),
+    "error" VARCHAR(2083),
+    "created" TIMESTAMP(6),
+    "updated" TIMESTAMP(6),
+    "channel" VARCHAR(255),
+    "version_code" INTEGER,
+    "artifact_url_base" VARCHAR(2083),
+    "artifact_files" VARCHAR(4096),
+    "build_guid" VARCHAR(255),
+    "console_text_url" VARCHAR(255),
+    "codebuild_url" VARCHAR(255),
+    "targets" VARCHAR(255),
+    "environment" TEXT,
 
-    INDEX `fk_build_job_id`(`job_id`),
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- CreateTable
-CREATE TABLE `client` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `access_token` VARCHAR(255) NOT NULL,
-    `prefix` VARCHAR(4) NOT NULL,
-    `created` DATETIME(0) NULL,
-    `updated` DATETIME(0) NULL,
-
-    INDEX `idx_accesS_token`(`access_token`),
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+    CONSTRAINT "build_pkey" PRIMARY KEY ("id")
+);
 
 -- CreateTable
-CREATE TABLE `email_queue` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `to` VARCHAR(255) NOT NULL,
-    `cc` VARCHAR(255) NULL,
-    `bcc` VARCHAR(255) NULL,
-    `subject` VARCHAR(255) NOT NULL,
-    `text_body` TEXT NULL,
-    `html_body` TEXT NULL,
-    `attempts_count` BOOLEAN NULL,
-    `last_attempt` DATETIME(0) NULL,
-    `created` DATETIME(0) NULL,
-    `error` VARCHAR(255) NULL,
+CREATE TABLE "public"."client" (
+    "id" SERIAL NOT NULL,
+    "access_token" VARCHAR(255) NOT NULL,
+    "prefix" VARCHAR(4) NOT NULL,
+    "created" TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
+    "updated" TIMESTAMP(6),
 
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+    CONSTRAINT "client_pkey" PRIMARY KEY ("id")
+);
 
 -- CreateTable
-CREATE TABLE `job` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `request_id` VARCHAR(255) NOT NULL,
-    `git_url` VARCHAR(2083) NOT NULL,
-    `app_id` VARCHAR(255) NOT NULL,
-    `publisher_id` VARCHAR(255) NOT NULL,
-    `created` DATETIME(0) NULL,
-    `updated` DATETIME(0) NULL,
-    `client_id` INTEGER NULL,
-    `existing_version_code` INTEGER NULL DEFAULT 0,
-    `jenkins_build_url` VARCHAR(1024) NULL,
-    `jenkins_publish_url` VARCHAR(1024) NULL,
+CREATE TABLE "public"."job" (
+    "id" SERIAL NOT NULL,
+    "request_id" VARCHAR(255) NOT NULL,
+    "git_url" VARCHAR(2083) NOT NULL,
+    "app_id" VARCHAR(255) NOT NULL,
+    "publisher_id" VARCHAR(255) NOT NULL,
+    "created" TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
+    "updated" TIMESTAMP(6),
+    "client_id" INTEGER,
+    "existing_version_code" INTEGER DEFAULT 0,
+    "jenkins_build_url" VARCHAR(1024),
+    "jenkins_publish_url" VARCHAR(1024),
 
-    INDEX `fk_job_client_id`(`client_id`),
-    INDEX `idx_request_id`(`request_id`),
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+    CONSTRAINT "job_pkey" PRIMARY KEY ("id")
+);
 
 -- CreateTable
-CREATE TABLE `migration` (
-    `version` VARCHAR(180) NOT NULL,
-    `apply_time` INTEGER NULL,
+CREATE TABLE "public"."project" (
+    "id" SERIAL NOT NULL,
+    "status" VARCHAR(255),
+    "result" VARCHAR(255),
+    "error" VARCHAR(2083),
+    "url" VARCHAR(1024),
+    "user_id" VARCHAR(255),
+    "group_id" VARCHAR(255),
+    "app_id" VARCHAR(255),
+    "project_name" VARCHAR(255),
+    "language_code" VARCHAR(255),
+    "publishing_key" VARCHAR(1024),
+    "created" TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
+    "updated" TIMESTAMP(6),
+    "client_id" INTEGER,
 
-    PRIMARY KEY (`version`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- CreateTable
-CREATE TABLE `operation_queue` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `operation` VARCHAR(255) NOT NULL,
-    `operation_object_id` INTEGER NULL,
-    `operation_parms` VARCHAR(2048) NULL,
-    `attempt_count` INTEGER NOT NULL,
-    `last_attempt` DATETIME(0) NULL,
-    `try_after` DATETIME(0) NULL,
-    `start_time` DATETIME(0) NULL,
-    `last_error` VARCHAR(2048) NULL,
-    `created` DATETIME(0) NULL,
-    `updated` DATETIME(0) NULL,
-
-    INDEX `idx_start_time`(`start_time`),
-    INDEX `idx_try_after`(`try_after`),
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+    CONSTRAINT "project_pkey" PRIMARY KEY ("id")
+);
 
 -- CreateTable
-CREATE TABLE `project` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `status` VARCHAR(255) NULL,
-    `result` VARCHAR(255) NULL,
-    `error` VARCHAR(2083) NULL,
-    `url` VARCHAR(1024) NULL,
-    `user_id` VARCHAR(255) NULL,
-    `group_id` VARCHAR(255) NULL,
-    `app_id` VARCHAR(255) NULL,
-    `project_name` VARCHAR(255) NULL,
-    `language_code` VARCHAR(255) NULL,
-    `publishing_key` VARCHAR(1024) NULL,
-    `created` DATETIME(0) NULL,
-    `updated` DATETIME(0) NULL,
-    `client_id` INTEGER NULL,
+CREATE TABLE "public"."release" (
+    "id" SERIAL NOT NULL,
+    "build_id" INTEGER NOT NULL,
+    "status" VARCHAR(255),
+    "created" TIMESTAMP(6) DEFAULT CURRENT_TIMESTAMP,
+    "updated" TIMESTAMP(6),
+    "result" VARCHAR(255),
+    "error" VARCHAR(2083),
+    "channel" VARCHAR(255) NOT NULL,
+    "title" VARCHAR(30),
+    "defaultLanguage" VARCHAR(255),
+    "promote_from" VARCHAR(255),
+    "build_guid" VARCHAR(255),
+    "console_text_url" VARCHAR(255),
+    "codebuild_url" VARCHAR(255),
+    "targets" VARCHAR(255),
+    "environment" TEXT,
+    "artifact_url_base" VARCHAR(255),
+    "artifact_files" VARCHAR(255),
 
-    INDEX `fk_project_client_id`(`client_id`),
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+    CONSTRAINT "release_pkey" PRIMARY KEY ("id")
+);
 
--- CreateTable
-CREATE TABLE `release` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `build_id` INTEGER NOT NULL,
-    `status` VARCHAR(255) NULL,
-    `created` DATETIME(0) NULL,
-    `updated` DATETIME(0) NULL,
-    `result` VARCHAR(255) NULL,
-    `error` VARCHAR(2083) NULL,
-    `channel` VARCHAR(255) NOT NULL,
-    `title` VARCHAR(30) NULL,
-    `defaultLanguage` VARCHAR(255) NULL,
-    `promote_from` VARCHAR(255) NULL,
-    `build_guid` VARCHAR(255) NULL,
-    `console_text_url` VARCHAR(255) NULL,
-    `codebuild_url` VARCHAR(255) NULL,
-    `targets` VARCHAR(255) NULL,
-    `environment` TEXT NULL,
-    `artifact_url_base` VARCHAR(255) NULL,
-    `artifact_files` VARCHAR(255) NULL,
+-- CreateIndex
+CREATE INDEX "idx_build_job_id" ON "public"."build"("job_id");
 
-    INDEX `fk_release_build_id`(`build_id`),
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- CreateIndex
+CREATE INDEX "idx_accesS_token" ON "public"."client"("access_token");
+
+-- CreateIndex
+CREATE INDEX "idx_job_client_id" ON "public"."job"("client_id");
+
+-- CreateIndex
+CREATE INDEX "idx_request_id" ON "public"."job"("request_id");
+
+-- CreateIndex
+CREATE INDEX "idx_project_client_id" ON "public"."project"("client_id");
+
+-- CreateIndex
+CREATE INDEX "idx_release_build_id" ON "public"."release"("build_id");
 
 -- AddForeignKey
-ALTER TABLE `build` ADD CONSTRAINT `fk_build_job_id` FOREIGN KEY (`job_id`) REFERENCES `job`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE "public"."build" ADD CONSTRAINT "fk_build_job_id" FOREIGN KEY ("job_id") REFERENCES "public"."job"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 -- AddForeignKey
-ALTER TABLE `job` ADD CONSTRAINT `fk_job_client_id` FOREIGN KEY (`client_id`) REFERENCES `client`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE "public"."job" ADD CONSTRAINT "fk_job_client_id" FOREIGN KEY ("client_id") REFERENCES "public"."client"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 -- AddForeignKey
-ALTER TABLE `project` ADD CONSTRAINT `fk_project_client_id` FOREIGN KEY (`client_id`) REFERENCES `client`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+ALTER TABLE "public"."project" ADD CONSTRAINT "fk_project_client_id" FOREIGN KEY ("client_id") REFERENCES "public"."client"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 -- AddForeignKey
-ALTER TABLE `release` ADD CONSTRAINT `fk_release_build_id` FOREIGN KEY (`build_id`) REFERENCES `build`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
-
+ALTER TABLE "public"."release" ADD CONSTRAINT "fk_release_build_id" FOREIGN KEY ("build_id") REFERENCES "public"."build"("id") ON DELETE NO ACTION ON UPDATE NO ACTION;
