752b185f6a58fbfdd1642426b8ea228283b7b6ac
Aidan Jones <aidan_jones@sil.org>
2025-12-18 09:34:34 -0600

    Delete auth file

--- src/auth.ts
+++
@@ -1,9 +0,0 @@
-import { type SvelteKitAuthConfig } from '@auth/sveltekit';
-
-const config: SvelteKitAuthConfig = {
-  trustHost: true,
-  providers: [{}],
-  session: {
-    maxAge: 60 * 60 * 24 // 24 hours
-  }
-};
