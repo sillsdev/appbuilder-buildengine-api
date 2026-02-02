0d1303583ac1335a89b9e45c968911a2dbf38625
Aidan Jones <aidan_jones@sil.org>
2026-02-02 10:42:26 -0600

    Use Scriptoria for Authentication (#72)
    * Install packages
    
    * Initial (non-functional) config
    
    * Move UI to group folder
    
    * Delete auth file
    
    * Use APP_ENV in queue prefix
    
    * Return stub info for versions
    
    * Move favicon/css import to root layout
    
    * Uninstall authjs, use jose directly
    
    * Basic authentication through Scriptoria
    
    * Add UI for login/signout
    
    * Update note in package.json
    
    * Enforce scope=admin on returned token

---
+++ src/routes/(auth)/exchange/+server.ts
@@ -0,0 +1,60 @@
+import { error } from 'console';
+import { EncryptJWT, jwtVerify } from 'jose';
+import type { RequestHandler } from './$types';
+import { env as secrets } from '$env/dynamic/private';
+import { env } from '$env/dynamic/public';
+import { returnTo } from '$lib/server/auth';
+import { QueueConnected } from '$lib/server/bullmq';
+import { getAuthConnection } from '$lib/server/bullmq/queues';
+
+// GET system/check
+export const GET: RequestHandler = async (event) => {
+  if (QueueConnected()) {
+    const requestId = event.url.searchParams.get('requestId');
+    const code = event.url.searchParams.get('code');
+    if (!requestId || !code) {
+      throw error(400, 'Missing URL Search Params');
+    }
+
+    const verify = await getAuthConnection().get(requestId);
+    if (!verify) {
+      throw error(400, 'Invalid or expired code');
+    }
+
+    try {
+      //immediately invalidate
+      await getAuthConnection().del(requestId);
+    } catch {
+      /* empty */
+    }
+
+    const res: { id_token?: string } = await fetch(
+      `${env.PUBLIC_SCRIPTORIA_URL}/api/auth/exchange`,
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          code,
+          verify
+        })
+      }
+    ).then((r) => r.json());
+
+    if (!res.id_token) {
+      throw error(401, 'Authentication failed');
+    }
+
+    const key = new TextEncoder().encode(secrets.AUTH0_SECRET);
+
+    const token = await jwtVerify(res.id_token, key);
+
+    const encryptedToken = await new EncryptJWT(token.payload)
+      .setProtectedHeader({ alg: 'dir', enc: 'A256CBC-HS512' })
+      .encrypt(key);
+
+    event.cookies.set('scriptoria.session-token', encryptedToken, { path: '/' });
+
+    throw returnTo(event);
+  } else {
+    throw error(503, 'Service Unavailable');
+  }
+};
