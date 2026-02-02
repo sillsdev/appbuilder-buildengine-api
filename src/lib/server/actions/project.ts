0841c3c5ca8af1daf2075133612dfff3099b00d1
Aidan Jones <aidan_jones@sil.org>
2026-02-02 16:36:08 -0600

    Finish translating project action

--- src/lib/server/actions/project.ts
+++ src/lib/server/actions/project.ts
@@ -2,13 +2,11 @@ import type { Prisma } from '@prisma/client';
 import { IAmWrapper } from '../aws/iamwrapper';
 import { prisma } from '../prisma';
 import { Utils } from '../utils';
+import { Project } from '$lib/models/project';
 
 export class ProjectUpdateOperation {
   private id;
   private parms;
-  private maxRetries = 50;
-  private maxDelay = 30;
-  private alertAfter = 5;
   public iAmWrapper; // Public for ut
 
   public constructor(id: number, parms: string) {
@@ -18,7 +16,10 @@ export class ProjectUpdateOperation {
   }
   public async performOperation() {
     console.log(`[${Utils.getPrefix()}] ProjectUpdateOperation ID: ${this.id}`);
-    const project = await prisma.project.findUnique({ where: { id: this.id } });
+    const project = await prisma.project.findUnique({
+      where: { id: this.id },
+      include: { client: true }
+    });
     if (project) {
       console.log('Found record');
       const parmsArray = this.parms.split(',');
@@ -30,15 +31,6 @@ export class ProjectUpdateOperation {
       console.log("Didn't find record");
     }
   }
-  public getMaximumRetries() {
-    return this.maxRetries;
-  }
-  public getMaximumDelay() {
-    return this.maxDelay;
-  }
-  public getAlertAfterAttemptCount() {
-    return this.alertAfter;
-  }
   /**
    * If the user/group combination associated with the current project is
    * the only project that exists, then remove the IAM user from the IAM group
@@ -47,7 +39,7 @@ export class ProjectUpdateOperation {
    * @return void
    */
   private async checkRemoveUserFromGroup(
-    project: Prisma.projectGetPayload<{ select: { user_id: true; group_id: true } }>
+    project: Prisma.projectGetPayload<{ select: { user_id: true } }> & Project.ProjectGroup
   ) {
     console.log('checkRemoveUserFromGroup');
     const projects = await prisma.project.count({
@@ -56,19 +48,19 @@ export class ProjectUpdateOperation {
     if (projects < 2) {
       // Remove the user from the group
       console.log(
-        `CheckRemoveUserFromGroup: Removing [${project.user_id}] from group [${project.groupName()}]`
+        `CheckRemoveUserFromGroup: Removing [${project.user_id}] from group [${Project.groupName(project)}]`
       );
-      this.iAmWrapper.removeUserFromIamGroup(project.user_id!, project.groupName());
+      this.iAmWrapper.removeUserFromIamGroup(project.user_id!, Project.groupName(project));
     }
   }
   private async updateProject(
-    project: Prisma.projectGetPayload<{ select: { id: true; url: true } }>,
+    project: Prisma.projectGetPayload<{ select: { id: true; url: true } }> & Project.ProjectGroup,
     user_id: string,
     publishing_key: string
   ) {
     console.log('updateProject');
     await this.iAmWrapper.createAwsAccount(user_id);
-    this.iAmWrapper.addUserToIamGroup(user_id, project.groupName());
+    this.iAmWrapper.addUserToIamGroup(user_id, Project.groupName(project));
     const public_key = await this.iAmWrapper.addPublicSshKey(user_id, publishing_key);
     const publicKeyId = public_key?.SSHPublicKey?.SSHPublicKeyId;
     const url = this.adjustUrl(project.url!, publicKeyId!);
