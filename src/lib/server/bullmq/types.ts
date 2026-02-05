6ea4b2df3bcd1eda2ec1f2c6723eef065f67ed72
Aidan Jones <aidan_jones@sil.org>
2026-02-05 10:51:37 -0600

    Move app version check to recurring job

--- src/lib/server/bullmq/types.ts
+++ src/lib/server/bullmq/types.ts
@@ -21,7 +21,8 @@ export enum QueueName {
   Projects = 'Projects',
   Releases = 'Releases',
   Polling = 'Polling',
-  System_Startup = 'System (Startup)'
+  System_Startup = 'System (Startup)',
+  System_Recurring = 'System (Recurring)'
 }
 
 export enum JobType {
@@ -38,7 +39,12 @@ export enum JobType {
   S3_CopyArtifacts = 'Copy Artifacts to S3',
   S3_CopyError = 'Copy Errors to S3',
   // System Jobs
-  System_CreateCodeBuildProject = 'Create CodeBuild Project'
+  System_CreateCodeBuildProject = 'Create CodeBuild Project',
+  System_RefreshAppVersions = 'Refresh AppVersions'
+}
+
+export enum JobSchedulerId {
+  RefreshAppVersions = 'RefreshAppVersions'
 }
 
 export namespace Build {
@@ -96,6 +102,9 @@ export namespace System {
   export interface CreateCodeBuildProject {
     type: JobType.System_CreateCodeBuildProject;
   }
+  export interface RefreshAppVersions {
+    type: JobType.System_RefreshAppVersions;
+  }
 }
 
 export type Job = JobTypeMap[keyof JobTypeMap];
@@ -105,7 +114,10 @@ export type S3Job = JobTypeMap[JobType.S3_CopyArtifacts | JobType.S3_CopyError];
 export type PublishJob = JobTypeMap[JobType.Release_Product];
 export type PollJob = JobTypeMap[JobType.Poll_Build | JobType.Poll_Release];
 export type ProjectJob = JobTypeMap[JobType.Project_Create];
-export type SystemJob = JobTypeMap[JobType.System_CreateCodeBuildProject];
+export type StartupJob = JobTypeMap[
+  | JobType.System_CreateCodeBuildProject
+  | JobType.System_RefreshAppVersions];
+export type RecurringJob = JobTypeMap[JobType.System_RefreshAppVersions];
 
 export type JobTypeMap = {
   [JobType.Build_Product]: Build.Product;
@@ -116,5 +128,6 @@ export type JobTypeMap = {
   [JobType.S3_CopyArtifacts]: S3.CopyArtifacts;
   [JobType.S3_CopyError]: S3.CopyErrors;
   [JobType.System_CreateCodeBuildProject]: System.CreateCodeBuildProject;
+  [JobType.System_RefreshAppVersions]: System.RefreshAppVersions;
   // Add more mappings here as needed
 };
