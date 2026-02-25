8378fc4fa71f1d5883a690d97efe913448691c07
Aidan Jones <aidan_jones@sil.org>
2026-02-25 14:04:40 -0600

    Various fixes

--- src/routes/(api)/system/check/+server.ts
+++ src/routes/(api)/system/check/+server.ts
@@ -8,9 +8,9 @@ export const GET: RequestHandler = async () => {
   return new Response(
     JSON.stringify({
       versions: Object.fromEntries(versions.map(({ appName, version }) => [appName, version])),
-      created: versions[0].created,
-      updated: versions[0].updated,
-      imageHash: versions[0].imageHash,
+      created: versions.at(0)?.created,
+      updated: versions.at(0)?.updated,
+      imageHash: versions.at(0)?.imageHash,
       _links: {
         self: {
           href: `${process.env.ORIGIN || 'http://localhost:8443'}/system/check`
