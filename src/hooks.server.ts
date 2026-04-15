1f776753eb763fa266ed1d7fd5964fce37332551
Aidan Jones <aidan_jones@sil.org>
2026-04-15 14:59:16 -0500

    CR feedback

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -10,7 +10,7 @@ import { allWorkers } from '$lib/server/bullmq/BullMQ';
 import { DatabaseConnected } from '$lib/server/prisma';
 
 const handleAPIRoute: Handle = async ({ event, resolve }) => {
-  if (event.route.id?.split('/')?.[2] === 'health') return resolve(event);
+  if (event.route.id === '/(api)/health') return resolve(event);
   const [success, res] = await tryVerifyAPIToken(event);
   if (!success) {
     return res;
