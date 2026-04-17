9455a0949b62f852e879a04a58e920ed92f919b9
Aidan Jones <aidan_jones@sil.org>
2026-04-17 15:48:16 -0500

    Improve client list display

--- src/lib/utils/time.ts
+++ src/lib/utils/time.ts
@@ -1,13 +1,64 @@
-type DateType = Date | string | null;
+import { type Readable, readable } from 'svelte/store';
 
+const locale = 'en-US';
+const relativeTimeFormatter = new Intl.RelativeTimeFormat(locale);
+
+type DateType = Date | string | null;
+export function getRelativeTime(date: DateType): Readable<string>;
+export function getRelativeTime(date: DateType[]): Readable<string[]>;
+export function getRelativeTime(date: DateType | DateType[]): Readable<string | string[]> {
+  if (Array.isArray(date)) {
+    return readable(
+      date.map((d) => getRelativeTimeValue(d)),
+      (set) => {
+        const interval = setInterval(() => {
+          set(date.map((d) => getRelativeTimeValue(d)));
+        }, 1000);
+        return () => clearInterval(interval);
+      }
+    );
+  }
+  return readable(getRelativeTimeValue(date), (set) => {
+    const interval = setInterval(() => {
+      set(getRelativeTimeValue(date));
+    }, 1000);
+    return () => clearInterval(interval);
+  });
+}
+export function getRelativeTimeValue(date: DateType): string {
+  if (typeof date === 'string') {
+    date = new Date(date);
+  }
+  if (!date) return '-';
+  // in miliseconds
+  const units = {
+    year: 24 * 60 * 60 * 1000 * 365,
+    month: (24 * 60 * 60 * 1000 * 365) / 12,
+    day: 24 * 60 * 60 * 1000,
+    hour: 60 * 60 * 1000,
+    minute: 60 * 1000,
+    second: 1000
+  } as const;
+
+  const elapsed = date.valueOf() - new Date().valueOf();
+  for (const u in units) {
+    if (Math.abs(elapsed) > units[u as keyof typeof units] || u == 'second') {
+      const ret = relativeTimeFormatter.format(
+        Math.round(elapsed / units[u as keyof typeof units]),
+        u as keyof typeof units
+      );
+      return ret;
+    }
+  }
+  return 'ERROR';
+}
 export function getTimeDateString(date: DateType): string {
   if (typeof date === 'string') {
     date = new Date(date);
   }
-  // using en-US as locale until we need to support other locales
-  return `${date?.toLocaleDateString('en-US') ?? '-'} ${
+  return `${date?.toLocaleDateString(locale) ?? '-'} ${
     date
-      ?.toLocaleTimeString('en-US', {
+      ?.toLocaleTimeString(locale, {
         hour: 'numeric',
         minute: '2-digit',
         hour12: true
--- src/routes/(ui)/client-admin/+page.svelte
+++ src/routes/(ui)/client-admin/+page.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
   import { type FormResult, superForm } from 'sveltekit-superforms';
   import type { PageData } from './$types';
-  import { enhance } from '$app/forms';
   import Breadcrumbs from '$lib/components/Breadcrumbs.svelte';
   import Pagination from '$lib/components/Pagination.svelte';
-  import SortTable from '$lib/components/SortTable.svelte';
+  import SecureDisplay from '$lib/components/SecureDisplay.svelte';
+  import Tooltip from '$lib/components/Tooltip.svelte';
   import { Icons } from '$lib/icons';
   import IconContainer from '$lib/icons/IconContainer.svelte';
   import { title } from '$lib/stores';
-  import { getTimeDateString } from '$lib/utils/time';
+  import { getRelativeTime, getTimeDateString } from '$lib/utils/time';
 
   $title = 'Clients';
 
@@ -20,11 +20,7 @@
 
   let clients = $state(data.clients);
 
-  const {
-    form,
-    enhance: pageEnhance,
-    submit
-  } = superForm(data.form, {
+  const { form, enhance, submit } = superForm(data.form, {
     dataType: 'json',
     resetForm: false,
     onChange() {
@@ -39,6 +35,9 @@
       }
     }
   });
+
+  const created = $derived(getRelativeTime(clients.map((c) => c.created)));
+  const updated = $derived(getRelativeTime(clients.map((c) => c.updated)));
 </script>
 
 <div class="w-full">
@@ -59,82 +58,41 @@
     <b>{data.count}</b>
     items
   </p>
-  <SortTable
-    data={clients}
-    columns={[
-      {
-        id: 'index',
-        header: '#'
-      },
-      {
-        id: 'id',
-        header: 'Id',
-        compare: () => 0
-      },
-      {
-        id: 'access_token',
-        header: 'Access Token',
-        compare: () => 0
-      },
-      {
-        id: 'prefix',
-        header: 'Prefix',
-        compare: () => 0
-      },
-      {
-        id: 'created',
-        header: 'Created',
-        compare: () => 0
-      },
-      {
-        id: 'updated',
-        header: 'Updated',
-        compare: () => 0
-      },
-      {
-        id: 'menu',
-        header: ''
-      }
-    ]}
-    serverSide={true}
-    startDesc={true}
-    onSort={(field, direction) => form.update((data) => ({ ...data, sort: { field, direction } }))}
-  >
-    {#snippet row(client, index)}
-      <tr>
-        <td>{index + $form.page.page * $form.page.size + 1}</td>
-        <td>{client.id}</td>
-        <td>{client.access_token}</td>
-        <td>{client.prefix}</td>
-        <td>{getTimeDateString(client.created)}</td>
-        <td>{getTimeDateString(client.updated)}</td>
-        <td class="flex flex-row flex-wrap p-1 space-x-2">
-          <a href="/client-admin/view?id={client.id}">
-            <IconContainer icon={Icons.View} width={16} />
-          </a>
-          <a href="/client-admin/update?id={client.id}">
-            <IconContainer icon={Icons.Edit} width={16} />
-          </a>
-          <form action="?/deleteClient" method="POST" use:enhance>
-            <input type="hidden" name="id" value={client.id} />
-            <button
-              type="button"
-              class="cursor-pointer"
-              onclick={(e) => {
-                if (confirm('Are you sure you want to delete this item?')) {
-                  (e.currentTarget.parentElement as HTMLFormElement).requestSubmit();
-                  clients = clients.filter((c) => c.id !== client.id);
-                }
-              }}
-            >
-              <IconContainer icon={Icons.Delete} width={16} />
-            </button>
-          </form>
-        </td>
-      </tr>
-    {/snippet}
-  </SortTable>
-  <form method="POST" action="?/page" use:pageEnhance>
+  <div class="flex flex-col gap-2">
+    {#each clients as client, i}
+      <div class="border rounded-md p-2 flex flex-col gap-1">
+        <div class="flex flex-row">
+          <h3 class="grow">
+            <a class="link" href="/client-admin/view?id={client.id}">#{client.id}</a>
+            <i class="pl-2">{client.prefix}</i>
+          </h3>
+          <div class="flex flex-col">
+            <Tooltip class="indent-0" tip={getTimeDateString(client.created)}>
+              Created: {$created[i]}
+            </Tooltip>
+            <Tooltip class="indent-0" tip={getTimeDateString(client.updated)}>
+              Updated: {$updated[i]}
+            </Tooltip>
+          </div>
+        </div>
+        <div class="flex flex-row items-center gap-x-1 w-full">
+          <IconContainer icon={Icons.Key} width={16} />
+          <SecureDisplay value={client.access_token} />
+        </div>
+        <div class="flex flex-row flex-wrap gap-1 w-full">
+          <div class="flex flex-row items-center gap-x-1">
+            <IconContainer icon={Icons.Project} width={16} />
+            {client._count.project}
+          </div>
+          <div class="flex flex-row items-center gap-x-1">
+            <IconContainer icon={Icons.Product} width={16} />
+            {client._count.job}
+          </div>
+        </div>
+      </div>
+    {/each}
+  </div>
+  <form method="POST" action="?/page" use:enhance>
     <div class="space-between-4 flex w-full flex-row flex-wrap place-content-start gap-1 p-4">
       <Pagination bind:size={$form.page.size} total={data.count} bind:page={$form.page.page} />
     </div>
