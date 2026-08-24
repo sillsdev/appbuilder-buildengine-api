81a294cbdefcaf5eccc1506d46a496da5f340145
Aidan Jones <aidan_jones@sil.org>
2026-08-24 14:58:25 -0500

    Fix client prefix field width

--- src/routes/(ui)/client-admin/update/+page.svelte
+++ src/routes/(ui)/client-admin/update/+page.svelte
@@ -40,7 +40,7 @@
   <div class="input-fields">
     <LabeledFormInput
       label="Prefix"
-      class="md:w-24!"
+      class="md:w-28!"
       input={{
         maxlength: stringLimits.client.prefix,
         icon: Icons.Folder,
@@ -102,7 +102,7 @@
     }
 
     .complement-width {
-      width: calc(100% - var(--spacing) * 24) /* 6rem = 96px */;
+      width: calc(100% - var(--spacing) * 28) /* 7rem = 112px */;
     }
   }
 </style>
